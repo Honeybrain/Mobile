2c8bb8d0bc65910bdeb8f6f64bda2583057befff
valentinbreiz <valentinbreiz@gmail.com>
2023-12-23 23:58:34 +0100

    🐛 Fix GRPC client

--- AppHoneyBrain/App.tsx
+++ AppHoneyBrain/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './Nav/AppNavigator';
 
+import '@stardazed/streams-polyfill';
+import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding'; // Requires peer dependency `text-encoding`
+polyfillEncoding();
+
 export default function App() {
   return (
     <NavigationContainer>
